de64f8d5b9355950ab49b20459fa9c85093fae1c
Maximiliano Sosa <msosa_@hotmail.com>
2021-08-25 16:39:15 -0300

    base

--- C1D1/Program.cs
+++ C1D1/Program.cs
@@ -7,6 +7,11 @@ namespace C1D1
         static void Main(string[] args)
         {
             Console.WriteLine("Hola a todos!");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hola a todos!");
+            }
         }
     }
 }
